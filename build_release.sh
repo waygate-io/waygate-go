f999747f7eff834b4cd51891d0fc78ecac0bdfa1
Anders Pitman <&ers@apitman.com>
2024-06-26 07:04:09 -0600

    Strip release build

--- build_release.sh
+++ build_release.sh
@@ -1,1 +1,2 @@
-CGO_ENABLED=1 go build -ldflags="-extldflags=-static -X main.Version=$(git describe --tags)" -tags sqlite_omit_load_extension,netgo,osusergo
+CGO_ENABLED=1 go build -ldflags="-extldflags=-static -X main.Version=$(git describe --tags)" -tags sqlite_omit_load_extension,netgo,osusergo -o waygate-server
+strip waygate-server
