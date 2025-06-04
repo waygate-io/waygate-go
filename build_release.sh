16c8e43fe2c5cdb5adfc4cdcb48308c7ef5ee875
Anders Pitman <&ers@apitman.com>
2025-06-04 16:12:34 -0600

    Update build script

--- build_release.sh
+++ build_release.sh
@@ -1,2 +1,2 @@
-CGO_ENABLED=1 go build -ldflags="-extldflags=-static -X main.Version=$(git describe --tags)" -tags sqlite_omit_load_extension,netgo,osusergo -o waygate-server
-strip waygate-server
+CGO_ENABLED=1 go build -ldflags="-extldflags=-static -X main.Version=$(git describe --tags)" -tags sqlite_omit_load_extension,netgo,osusergo -o waygate
+strip waygate
